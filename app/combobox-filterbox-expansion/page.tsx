c29e118dafb28681b2c62ac63c9bf98828b49272
Jon Hogendorp <jonhoog@live.nl>
2024-04-09 10:15:21 +0200

    Drop down expansion

---
+++ app/combobox-filterbox-expansion/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ComboBoxFilters from './ComboBoxFilters';
+
+export default function FilterBox(){
+  return (
+    <div className='flex justify-center items-center vh-80 w-full'>
+        <ComboBoxFilters id={0} name={''} />
+    </div>
+  )
+}
+
