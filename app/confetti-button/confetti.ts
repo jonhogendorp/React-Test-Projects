3e89bb4d0d83baf88e09f01eaaffeacb5a006e3c
Jon Hogendorp <jonhoog@live.nl>
2024-06-26 09:45:53 +0200

    Confetti test

---
+++ app/confetti-button/confetti.ts
@@ -0,0 +1,107 @@
+import gsap from 'gsap';
+import { Physics2DPlugin } from 'gsap/Physics2DPlugin';
+
+gsap.registerPlugin(Physics2DPlugin);
+
+// document.querySelectorAll('.button').forEach((button: Element) => {
+//   const bounding = button.getBoundingClientRect();
+
+//   button.addEventListener('mousemove', (e: MouseEvent) => {
+//     let dy = (e.clientY - bounding.top - bounding.height / 2) / -1;
+//     let dx = (e.clientX - bounding.left - bounding.width / 2) / 10;
+
+//     dy = dy > 10 ? 10 : dy < -10 ? -10 : dy;
+//     dx = dx > 4 ? 4 : dx < -4 ? -4 : dx;
+
+//     button.style.setProperty('--rx', dy.toString());
+//     button.style.setProperty('--ry', dx.toString());
+//   });
+
+//   button.addEventListener('mouseleave', () => {
+//     button.style.setProperty('--rx', '0');
+//     button.style.setProperty('--ry', '0');
+//   });
+
+//   button.addEventListener('click', () => {
+//     button.classList.add('success');
+//     gsap.to(button, {
+//       '--icon-x': -3,
+//       '--icon-y': 3,
+//       '--z-before': 0,
+//       duration: 0.2,
+//       onComplete() {
+//         particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
+//         gsap.to(button, {
+//           '--icon-x': 0,
+//           '--icon-y': 0,
+//           '--z-before': -6,
+//           duration: 1,
+//           ease: 'elastic.out(1, .5)',
+//           onComplete() {
+//             button.classList.remove('success');
+//           },
+//         });
+//       },
+//     });
+//   });
+// });
+
+function particles(
+  parent: HTMLElement,
+  quantity: number,
+  x: number,
+  y: number,
+  minAngle: number,
+  maxAngle: number,
+) {
+  let colors = ['#FFFF04', '#EA4C89', '#892AB8', '#4AF2FD'];
+  for (let i = quantity - 1; i >= 0; i--) {
+    let angle = gsap.utils.random(minAngle, maxAngle);
+    let velocity = gsap.utils.random(70, 140);
+    let dot = document.createElement('div');
+    dot.style.setProperty('--b', colors[Math.floor(gsap.utils.random(0, 4))]);
+    parent.appendChild(dot);
+    gsap.set(dot, {
+      opacity: 0,
+      x: x,
+      y: y,
+      scale: gsap.utils.random(0.4, 0.7),
+    });
+    gsap
+      .timeline({
+        onComplete() {
+          dot.remove();
+        },
+      })
+      .to(
+        dot,
+        {
+          duration: 0.05,
+          opacity: 1,
+        },
+        0,
+      )
+      .to(
+        dot,
+        {
+          duration: 1.8,
+          rotationX: `-=${gsap.utils.random(720, 1440)}`,
+          rotationZ: `+=${gsap.utils.random(720, 1440)}`,
+          physics2D: {
+            angle: angle,
+            velocity: velocity,
+            gravity: 120,
+          },
+        },
+        0,
+      )
+      .to(
+        dot,
+        {
+          duration: 1,
+          opacity: 0,
+        },
+        0.8,
+      );
+  }
+}
